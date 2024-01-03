c53d1909f5089c862627824bfff2f28f121a2baf
sondresj <sondresj@pm.me>
2024-01-03 22:00:18 +0100

    ensure lsp not lazy loaded, and hook up capabilities

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,15 +1,17 @@
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = function()
             require("mason").setup()
         end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", },
+                ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "eslint" },
                 automatic_installation = {
                     exclude = { "lua_ls", "rust_analyzer", "tsserver", },
                 }
@@ -18,15 +20,23 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
             lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
                 settings = {
                     ['rust-analyzer'] = {}
                 }
             })
-            lspconfig.tsserver.setup({})
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
 
 
             vim.api.nvim_create_autocmd('LspAttach', {
