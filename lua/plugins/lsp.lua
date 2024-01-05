2be2c87663c697f29a2c7975d0de1e89b90fce22
Sondre Jensen <sondre.jensen@visma.com>
2024-01-05 12:58:00 +0100

    wip add proper support for csharp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { "lua_ls", "rust_analyzer", "tsserver", "eslint", "csharp_ls" }
+local servers = { "lua_ls", "rust_analyzer", "tsserver", "eslint", "csharp_ls", "omnisharp" }
 return {
     {
         "williamboman/mason.nvim",
@@ -24,12 +24,14 @@ return {
         priority = 51,
         dependencies = {
             'simrat39/rust-tools.nvim',
+            'Decodetalkers/csharpls-extended-lsp.nvim',
         },
         lazy = false,
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
 
+            -- set defaults for all servers
             for _, server in ipairs(servers) do
                 lspconfig[server].setup {
                     capabilities = capabilities,
@@ -41,6 +43,43 @@ return {
                     capabilities = capabilities,
                 }
             })
+           
+            local pid = vim.fn.getpid() 
+            local csharp_ls_bin = vim.fn.expand("$HOME") .. "/.dotnet/tools/csharp-ls.exe"
+            local omnisharp_bin = vim.fn.stdpath("data") .. "/mason/packages/omnisharp/libexec/OmniSharp.dll"
+
+            if vim.fn.has("macunix") then
+                local csharp_ls_bin = vim.fn.expand("$HOME") .. "/.dotnet/tools/csharp-ls" -- todo, need to verify this is correct on linux
+                local omnisharp_bin = vim.fn.stdpath("data") .. "/mason/bin/omnisharp.cmd"
+            end 
+
+            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#omnisharp
+            lspconfig.omnisharp.setup({
+                capabilities = capabilities,
+                handlers = {
+                    ["textDocument/definition"] = require("csharpls_extended").handler,
+                    ["textDocument/typeDefinition"] = require("csharpls_extended").handler,
+                },
+                cmd = { "dotnet", omnisharp_bin }, -- hopefully works on linux, mac and windows,
+                enable_editorconfig_support = true,
+                enable_ms_build_load_projects_on_demand = false,
+                enable_roslyn_analyzers = true,
+                organize_imports_on_format = true,
+                enable_import_completion = true,
+                sdk_include_prereleases = false,
+                analyze_open_documents_only = false,
+            })
+
+            -- https://github.com/Decodetalkers/csharpls-extended-lsp.nvim
+            lspconfig.csharp_ls.setup({
+                capabilities = capabilities,
+                handlers = {
+                    ["textDocument/definition"] = require("csharpls_extended").handler,
+                    ["textDocument/typeDefinition"] = require("csharpls_extended").handler,
+                },
+                cmd = { csharp_ls_bin, '--languageserver', '--hostPID', tostring(pid) }
+            })
+
 
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('UserLspConfig', {}),
